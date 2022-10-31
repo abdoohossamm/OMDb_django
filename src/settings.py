ff649eda408190bea9e59eeb07d7b8ab2a402489
abdoohossamm <abdoohossamm@outlook.com>
2022-10-31 19:57:37 +0200

    add logging settings

--- src/settings.py
+++ src/settings.py
@@ -127,6 +127,28 @@ class Dev(Configuration):
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            }
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
+
 
 class Prod(Dev):
     DEBUG = os.environ.get('DEBUG', 0)
