dbfb66a8f345dc6db6647805413d19cb102067da
abdoohossamm <abdoohossamm@outlook.com>
2022-11-01 23:34:28 +0200

    create required model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class SearchTerm(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    term = models.TextField(unique=True)
+    last_search = models.DateTimeField(auto_now=True)
+
+
+class Genre(models.Model):
+    class Meta:
+        ordering = ["name"]
+
+    name = models.TextField(unique=True)
+
+
+class Movie(models.Model):
+    class Meta:
+        ordering = ["title", "year"]
+
+    title = models.TextField()
+    year = models.PositiveIntegerField()
+    runtime_minutes = models.PositiveIntegerField(null=True)
+    imdb_id = models.SlugField(unique=True)
+    genres = models.ManyToManyField(Genre, related_name="movies")
+    plot = models.TextField(null=True, blank=True)
+    is_full_record = models.BooleanField(default=False)
