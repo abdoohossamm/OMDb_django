d7b6814a906a0e8a1f433268a562054e5d9a2033
abdoohossamm <abdoohossamm@outlook.com>
2022-11-07 23:18:03 +0200

    setup django signals

--- movies/apps.py
+++ movies/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'
+
+    def ready(self):
+        import movies.signals # noqa
---
+++ movies/signals.py
@@ -0,0 +1,17 @@
+"""
+Signals handlers module
+"""
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from movies.models import SearchTerm
+
+
+@receiver(post_save, sender=SearchTerm, dispatch_uid="search_term_saved")
+def search_term_saved(sender, instance, created, **kwargs):
+    """
+        Listener function is called when a SearchTerm is saved
+    """
+    if created:
+        # new SearchTerm was created
+        print(f"A new SearchTerm was created: '{instance.term}'")
